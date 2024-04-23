af57211a873c4fbc8b94093818846aea874106f5
mohamedl23 <mhmed.mostafa550@gmail.com>
2024-04-24 01:23:58 +0200

    Specification Design Pattern

---
+++ Talabat.Core/Specification/BaseSpecifiction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entitees;
+
+namespace Talabat.Core.Specification
+{
+	internal class BaseSpecifiction<T> : ISpecifiction<T> where T : BaseEntity
+	{
+		public Expression<Func<T, bool>> Criteria { get ; set ; }
+		public List<Expression<Func<T, object>>> Includes { get ; set ; } = new List<Expression<Func<T, object>>>();
+
+        public BaseSpecifiction()
+        {
+            
+        }
+        public BaseSpecifiction(Expression<Func<T ,bool>> CriteriaExpression )
+        {
+            Criteria = CriteriaExpression;
+        }
+
+    }
+}
